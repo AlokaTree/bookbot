b848aa91b8dae3b029465cbfe50534d338ecb559
AlokaTree <p_lemaistre@hotmail.com>
2025-01-30 11:35:59 +0000

    code restructure

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-#!/usr/bin/python3
-
 def main():
-    print("--- Begin report of books/frankenstein.txt ---")
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document" "\n")
     chars_dict = get_chars_dict(text)
-    letters_found = get_letters(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
     print("--- End report ---")
 
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 def get_chars_dict(text):
@@ -25,12 +43,6 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
-
-
-def get_letters(chars_dict):
-    for key, value in sorted(chars_dict.items()):
-        if key.isalpha() == True:
-            print(f"the '{key}' character was found {value} times")
 
 
 def get_book_text(path):
